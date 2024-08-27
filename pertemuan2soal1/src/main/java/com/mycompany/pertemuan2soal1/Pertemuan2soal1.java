646eac1dfabe1ed23de7aa61d790d10a983a3016
nurikasachikocallulaa <nurikasachikocallulaa0@gmail.com>
2024-08-27 12:34:27 +0700

    pertemuan 2 soal1 VARIABEL PADA JAVA

---
+++ pertemuan2soal1/src/main/java/com/mycompany/pertemuan2soal1/Pertemuan2soal1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan2soal1;
+
+/**
+ *
+ * @author pc
+ */
+public class Pertemuan2soal1 {
+
+    public static String Makanan;
+//Membuat variable tnapa value/Nilai pada class staticvariable
+    public static final String Minuman = "Jus Jambu";
+public static void main (String[] args) {
+        //TODO code aplication logic here
+  Makanan = "Ayam Geprek  ";//Mengisi Value pada variable Makanan
+  System.out.println("Makanan Favorit Saya "+ Makanan);//Memanggil Variable Makanan
+  System.out.println("Minuman Favorit Saya "+ Minuman);//Memanggil Variable Minuman
+    }
+}
