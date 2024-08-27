06384f076a72a172c350b3f4443bcb7619750de8
nurikasachikocallulaa <nurikasachikocallulaa0@gmail.com>
2024-08-27 13:50:13 +0700

    Pertemuan 9 soal3 MENGONTROL ALIRAN PROGRAM PERULANGAN WHILE, DO WHILE

---
+++ Pertemuan9soal3/src/main/java/com/mycompany/pertemuan9soal3/Pertemuan9soal3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan9soal3;
+
+/**
+ *
+ * @author pc
+ */
+public class Pertemuan9soal3 {
+
+    public static void main(String[] args) {
+         int i = 1;
+        while (i < 6) {
+            System.out.println(i);
+            i++; // Menambah nilai i agar loop tidak berjalan selamanya
+        }
+    }
+}
