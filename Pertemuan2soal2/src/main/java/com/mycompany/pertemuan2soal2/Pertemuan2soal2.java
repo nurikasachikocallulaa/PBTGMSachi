e8a7c84182a9e76f64f1340c6e9622cc0ffdf294
nurikasachikocallulaa <nurikasachikocallulaa0@gmail.com>
2024-08-27 12:48:50 +0700

    Pertemuan 2 soal2 VARIABEL PADA JAVA

---
+++ Pertemuan2soal2/src/main/java/com/mycompany/pertemuan2soal2/Pertemuan2soal2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan2soal2;
+
+/**
+ *
+ * @author pc
+ */
+public class Pertemuan2soal2 {
+
+    public static void main(String[] args) {
+        int a=50;
+float b=a;
+System.out.println(a);
+System.out.println(b);
+    }
+}
