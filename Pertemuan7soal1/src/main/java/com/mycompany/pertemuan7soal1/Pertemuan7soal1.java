91a547606a9ff35dd9c6d6a03fb4edb6b71a931c
nurikasachikocallulaa <nurikasachikocallulaa0@gmail.com>
2024-08-27 13:03:27 +0700

    Pertemuan 7 soal1 MENGONTROL ALIRAN PROGRAM PERNYATAAN SWITCH..CASE

---
+++ Pertemuan7soal1/src/main/java/com/mycompany/pertemuan7soal1/Pertemuan7soal1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan7soal1;
+
+/**
+ *
+ * @author pc
+ */
+public class Pertemuan7soal1 {
+
+    public static void main(String[] args) {
+            int day = 2;
+
+        switch (day) {
+            case 1:
+                System.out.println("Sabtu");
+                break;
+            case 2:
+                System.out.println("Minggu");
+                break;
+       }
+    }
+}
