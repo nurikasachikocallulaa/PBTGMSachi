2c9c041c06aac7d024f049baba90d200f3b82756
nurikasachikocallulaa <nurikasachikocallulaa0@gmail.com>
2024-08-27 12:58:42 +0700

    Pertemuan 5 soal2 INPUT OUTPUT STREAM AND KEYWORD PADA JAVA

---
+++ Pertemuan5soal2/src/main/java/com/mycompany/pertemuan5soal2/Pertemuan5soal2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan5soal2;
+
+/**
+ *
+ * @author pc
+ */
+public class Pertemuan5soal2 {
+
+    public static void main(String[] args) {
+        // Menetapkan nilai alas dan tinggi secara langsung 
+        double alas = 17;
+        double tinggi = 11;
+        
+        //Menghitung luas segitiga
+        double luas = (alas * tinggi)/2;
+        
+        // Menampilkan hasil perhitungan luas segitiga
+        System.out.println("Luas Segitiga dengan alas " + alas + " dan tinggi " + tinggi + " adalah: " + luas);
+    }
+}
