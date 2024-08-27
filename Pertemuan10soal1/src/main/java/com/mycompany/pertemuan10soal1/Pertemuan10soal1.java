3916aafc0037aa3d9403f9450e24caacc71337d9
nurikasachikocallulaa <nurikasachikocallulaa0@gmail.com>
2024-08-27 13:51:15 +0700

    Pertemuan 10 soal1 KONSEP PEMROGRAMAN BERORIENTASI OBJEK CLASS, OBJECT, ATTRIBUTE, METHOD

---
+++ Pertemuan10soal1/src/main/java/com/mycompany/pertemuan10soal1/Pertemuan10soal1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan10soal1;
+
+/**
+ *
+ * @author pc
+ */
+public class Pertemuan10soal1 {
+
+    public static void main(String[] args) {
+        Aritmatika aritmatika = new Aritmatika();
+
+        System.out.println("Pengurangan: " + aritmatika.pengurangan(10, 5));
+        System.out.println("Perkalian: " + aritmatika.perkalian(10, 5));
+        System.out.println("Pembagian: " + aritmatika.pembagian(10, 2));
+        System.out.println("Pangkat: " + aritmatika.pangkat(2, 3));
+    }
+}
