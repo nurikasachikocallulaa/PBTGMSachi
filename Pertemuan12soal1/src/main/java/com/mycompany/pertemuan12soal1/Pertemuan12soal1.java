b46ef9dd5b3d3d5fefcde0f087b64ddf43572b16
nurikasachikocallulaa <nurikasachikocallulaa0@gmail.com>
2024-08-27 13:55:20 +0700

    Pertemuan 12 soal1 KONSEP PEMROGRAMAN BERORIENTASI OBJEK  INHERITANTANCE/PEWARISAN

---
+++ Pertemuan12soal1/src/main/java/com/mycompany/pertemuan12soal1/Pertemuan12soal1.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan12soal1;
+
+/**
+ *
+ * @author pc
+ */
+class Karyawan {
+    String id;
+    String nama;
+    String jabatan;
+    double gajiPokok;
+    double tunjanganMakan;
+    double tunjanganTransport;
+
+    public Karyawan(String id, String nama, String jabatan, double gajiPokok, double tunjanganMakan, double tunjanganTransport) {
+        this.id = id;
+        this.nama = nama;
+        this.jabatan = jabatan;
+        this.gajiPokok = gajiPokok;
+        this.tunjanganMakan = tunjanganMakan;
+        this.tunjanganTransport = tunjanganTransport;
+    }
+}
+
+class KaryawanTetap extends Karyawan {
+    double bonus;
+
+
+    public KaryawanTetap(String id, String nama, String jabatan, double gajiPokok, double tunjanganMakan, double tunjanganTransport, double bonus) {
+        super(id, nama, jabatan, gajiPokok, tunjanganMakan, tunjanganTransport);
+        this.bonus = bonus;
+    }
+    public double hitungGajiTotal() {
+        return gajiPokok + bonus + tunjanganMakan + tunjanganTransport;
+    }
+
+    public void cetakDetailGaji() {
+        System.out.println("Gaji " + jabatan + " : " + gajiPokok);
+        System.out.println("Bonus : " + bonus);
+        System.out.println("Tunjangan Makan : " + tunjanganMakan);
+        System.out.println("Transport : " + tunjanganTransport);
+        System.out.println("Gaji Total : " + hitungGajiTotal());
+    }
+}
+public class Pertemuan12soal1 {
+
+    public static void main(String[] args) {
+        KaryawanTetap karyawan1 = new KaryawanTetap("001", "Budi", "System Analyst", 4000000, 400000, 500000, 1000000);
+
+        karyawan1.cetakDetailGaji();
+    }
+}
